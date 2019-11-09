7348036bc8fda3d6aaeb66a3be54ebc350bae214
Annie Zheng <anniezheng.anniee@gmail.com>
2019-11-09 04:38:09 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,43 +1,40 @@
 def new_hash
-  new_hash = Hash.new
+  {}
 end
 
 def my_hash
-  my_hash = { :key => "value"}
-  my_hash
+  # return a valid hash with any key/value pair of your choice
+  {nighttime: 'sleepy'}
 end
 
 def pioneer
-  pioneer = Hash.new
-  pioneer[:name] = "Grace Hopper"
-  pioneer
+  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  {:name => 'Grace Hopper'}
 end
 
 def id_generator
-  id_generator = Hash.new
-  id_generator[:id] = 74000
-  id_generator
+  # return a hash with a key :id assigned to positive integer
+  {id: 120}
 end
 
 def my_hash_creator(key, value)
-  my_hash_creator = Hash.new
-  my_hash_creator[key] = value
-  my_hash_creator
+  # return a hash that includes the key and value parameters passed into this method
+  {key => value}
 end
 
-def read_from_hash(hash, key = nil)
+def read_from_hash(hash, key)
+  # return the correct value using the hash and key parameters
   hash[key]
 end
 
 def update_counting_hash(hash, key)
-  if hash[key]
-    hash[key] +=1
-    hash
-  else 
-    hash[key] = 1
-    hash
-  end
-  # given a hash an a ey as parameters, return an updated hash
+  # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
+  if hash[key]
+    hash[key] += 1
+  else 
+    hash[key] = 1
+  end
+  hash
 end
